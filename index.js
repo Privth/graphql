e4b95739c58c35693c52543fd9eb541c384cea14
Wojciech Król <wojciechkrol97@gmail.com>
2020-03-08 13:16:05 +0100

    to review

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import  {makeExecutableSchema} from "graphql-tools";
+
+import resolvers from "./resolvers";
+import typeDefs from './schema'
+import models from  './db/models'
+
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    context: models,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
